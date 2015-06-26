0715de1afedbb49d236f478e0f76e64906f9515a
Messering <messering@yandex.ru>
2015-06-26 20:02:47 +0300

    controoller

--- core/src/com/mygdx/game/Main/Algo.java
+++ core/src/com/mygdx/game/Main/Algo.java
@@ -7,11 +7,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.Maze_Search.Recursive;
+import com.mygdx.game.Player.Player;
 import com.mygdx.game.Wall_Empty.Empty;
-import com.mygdx.game.Wall_Empty.Unit;
 import com.mygdx.game.Wall_Empty.Walk;
 import com.mygdx.game.Wall_Empty.Wall;
 
@@ -19,23 +18,23 @@ import com.mygdx.game.Wall_Empty.Wall;
 
 public class Algo extends ApplicationAdapter {
 	public static final int FIELD_SIZE = 13; //розмір нашого лабіринта, скілкьи на скілкьи буде наш масив
-	public static final float UPDATE_TIME = 0.0001f; /*швидкість обновлення лабіринут
+	public static final float UPDATE_TIME = 0.1f; /*швидкість обновлення лабіринут
 	, зараз застосовується при переміщенні камери
         */
 	SpriteBatch batch; //спрайти хто не розумію вам сюди  http://habrahabr.ru/post/159027/
 	OrthographicCamera camera;
 	Texture texture;
-	Rectangle player;
-	Texture texture_2;
 	Texture texture_1;
+	Player un;
+	int[][] bmap;
 
 
 	Cell[][] map;//масив який набирає параметірв кольору розміру квадрата , вся реалізація в класі СеІІ
+
 	public void create() { //так, це повинні знати, головний ініціалізатор входження
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera(FIELD_SIZE, FIELD_SIZE);
 		map = new Cell[FIELD_SIZE][FIELD_SIZE];
-		texture_2 = new Texture(Gdx.files.internal("title.jpg"));
 		texture = new Texture(Gdx.files.internal("mob.jpg"));
 		texture_1 = new Texture(Gdx.files.internal("way.jpg"));//вантажимо текстуру, картинки знаходять в проекті андроїд
 		// папка асерт і мають на неї автоматичне посилання у всіх проектах
@@ -48,52 +47,43 @@ public class Algo extends ApplicationAdapter {
 				};
 			}*/
 		//int[][] bmap = (new EllersAlgorithm(FIELD_SIZE, FIELD_SIZE)).generator();//будуєм масив
-		int[][] bmap = {
-				{1,1,1,1,1,1,1,1,1,1,1,1,1},
-				{1,0,1,0,0,0,1,0,0,0,0,0,1},
-				{1,0,1,0,1,0,1,0,1,0,1,0,1},
-				{1,0,1,0,1,1,1,0,1,0,1,1,1},
-				{0,0,0,0,0,0,0,0,1,0,0,0,1},
-				{1,0,1,1,1,1,1,0,1,1,1,1,1},
-				{1,0,0,0,0,0,1,0,0,0,0,0,1},
-				{1,1,0,1,1,1,1,1,0,1,0,1,1},
-				{1,1,0,0,0,0,0,1,0,1,0,1,1},
-				{1,0,1,1,0,1,0,0,0,0,1,1,1},
-				{1,0,0,1,0,1,0,1,1,0,0,0,1},
-				{1,1,0,0,0,0,0,0,1,0,1,0,1},
-				{1,1,1,1,1,1,1,1,1,0,1,1,1}};
-		for (int i = 0; i < FIELD_SIZE; i++)
-		{
+		bmap = new int[][]{
+				{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+				{1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+				{1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+				{1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1},
+				{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+				{1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+				{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+				{1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1},
+				{1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1},
+				{1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1},
+				{1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1},
+				{1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+				{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1}};
+		for (int i = 0; i < FIELD_SIZE; i++) {
 			for (int j = 0; j < FIELD_SIZE; j++) {
-			System.out.print(bmap[i][j]);
+				System.out.print(bmap[i][j]);
 			}
 			System.out.println();
 		}
 
 		MazeSearch(bmap);//In Maze searching & changing bmap drawing paths
 
-		// создается Rectangle для представления ведра
-		player = new Rectangle();
-		// центрируем ведро по горизонтали
-		player.x =  1;
-		// размещаем на 20 пикселей выше нижней границы экрана.
-		player.y = 1;
-		player.width = 4;
-		player.height =4;
 
-	//MAP CREATING
+		//MAP CREATING
 		for (int i = 0; i < FIELD_SIZE; i++)
 			for (int j = 0; j < FIELD_SIZE; j++) {
 				if (bmap[i][j] == 0)
-					map[i][j] = new Wall(texture_2); //стіна
+					map[i][j] = new Wall(texture); //стіна
 				if (bmap[i][j] == 1)
 					map[i][j] = new Empty(texture);//нуль прохід(клас емпті з пакету вол-емпті
 				if (bmap[i][j] == 2)
 					map[i][j] = new Walk(texture_1);//Обхід лабіринту
 			}
 		//MAZE SEARCH
+		un = new Player(bmap,map, 4, 0);
 
-		map[4][0]=new Unit(texture_2);
 		System.out.println();
 		for (int i = 0; i < FIELD_SIZE; i++) {
 			for (int j = 0; j < FIELD_SIZE; j++) {
@@ -102,11 +92,12 @@ public class Algo extends ApplicationAdapter {
 			System.out.println();
 		}
 	}
-	public void MazeSearch(int[][] bmap)
-	{
+
+	public void MazeSearch(int[][] bmap) {
 		Recursive Search = new Recursive(bmap, 4, 0);
 		Search.InMazeSearch();
 	}
+
 	@Override
 	public void render() { //рендеремо
 		this.update();
@@ -117,13 +108,14 @@ public class Algo extends ApplicationAdapter {
 
 		batch.begin();
 		for (int i = 0; i < FIELD_SIZE; i++)
-			for (int j = 0; j < FIELD_SIZE; j++){
+			for (int j = 0; j < FIELD_SIZE; j++) {
 				map[i][j].draw(batch, i, j);
 			}
 
 		batch.end();
 	}
-	public void update() {  //прописане керування але подумую щоб запхати в новий клас або пакет контроллер
+
+	public void update(){  //прописане керування але подумую щоб запхати в новий клас або пакет контроллер
 
 		float a = UPDATE_TIME;
 		Input input = Gdx.input;
@@ -132,39 +124,51 @@ public class Algo extends ApplicationAdapter {
 			for (int j = 0; j < FIELD_SIZE; j++)
 				map[i][j].update(map, i, j, texture);
 
-		if(input.isKeyPressed(Input.Keys.W))
+		/*if(input.isKeyPressed(Input.Keys.W))
 			camera.zoom-=Gdx.graphics.getDeltaTime();
 
 		if(input.isKeyPressed(Input.Keys.S))
-			camera.zoom+=Gdx.graphics.getDeltaTime();
+			camera.zoom+=Gdx.graphics.getDeltaTime();*/
 
-		if(input.isKeyPressed(Input.Keys.Q))
+		if (input.isKeyPressed(Input.Keys.Q))
 			camera.rotate(Gdx.graphics.getDeltaTime() * 90);
-		if(input.isKeyPressed(Input.Keys.E))
-			camera.rotate(-Gdx.graphics.getDeltaTime()*90);
+		if (input.isKeyPressed(Input.Keys.E))
+			camera.rotate(-Gdx.graphics.getDeltaTime() * 90);
 
-		if(input.isKeyPressed(Input.Keys.CONTROL_LEFT)) a = UPDATE_TIME + Gdx.graphics.getDeltaTime();
-		if(input.isKeyPressed(Input.Keys.SHIFT_LEFT)){
-			a = UPDATE_TIME - Gdx.graphics.getDeltaTime();}
+		if (input.isKeyPressed(Input.Keys.CONTROL_LEFT)) a = UPDATE_TIME + Gdx.graphics.getDeltaTime();
+		if (input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
+			a = UPDATE_TIME - Gdx.graphics.getDeltaTime();
+		}
 
-		if(input.isKeyPressed(Input.Keys.LEFT))
-			camera.translate(new Vector2(-Gdx.graphics.getDeltaTime()*50,0));
-		if(input.isKeyPressed(Input.Keys.RIGHT))
-			camera.translate(new Vector2(Gdx.graphics.getDeltaTime()*50,0));
-		if(input.isKeyPressed(Input.Keys.UP))
+		if (input.isKeyPressed(Input.Keys.LEFT))
+			camera.translate(new Vector2(-Gdx.graphics.getDeltaTime() * 50, 0));
+		if (input.isKeyPressed(Input.Keys.RIGHT))
+			camera.translate(new Vector2(Gdx.graphics.getDeltaTime() * 50, 0));
+		if (input.isKeyPressed(Input.Keys.UP))
 			camera.translate(new Vector2(0, Gdx.graphics.getDeltaTime() * 50));
-		if(input.isKeyPressed(Input.Keys.DOWN))
+		if (input.isKeyPressed(Input.Keys.DOWN))
 			camera.translate(new Vector2(0, -Gdx.graphics.getDeltaTime() * 50));
 
-		if(input.isKeyPressed(Input.Keys.SPACE)){
+		if (input.isKeyPressed(Input.Keys.SPACE)) {
 			a = 1f;
 			camera = new OrthographicCamera(FIELD_SIZE, FIELD_SIZE);
 		}
+		if (input.isKeyPressed(Input.Keys.W)){
+			un.up();
+
+		}
+		if (input.isKeyPressed(Input.Keys.S)) {
+			un.down();
+		}
+		if (input.isKeyPressed(Input.Keys.D)) {
+			un.right();
+		}
+		if (input.isKeyPressed(Input.Keys.A)) {
+			un.left();
+		}
+
 
 		camera.update();
-		if(input.isKeyPressed(Input.Keys.L))
-		{map[4][1]=new Unit(texture_2); map[4][0]=new Empty(texture_2);}
-
+			}
 	}
-}
 
