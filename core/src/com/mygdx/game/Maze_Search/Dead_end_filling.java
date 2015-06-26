f514360d49d2e95f2808bfa3786615c904fcec1b
VoidHikka <ollkokun@gmai.com>
2015-06-27 00:56:37 +0300

    NewMazeSearchingTools

---
+++ core/src/com/mygdx/game/Maze_Search/Dead_end_filling.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.Maze_Search;
+
+/**
+ * Created by Ollko_000 on 6/26/2015.
+ */
+public class Dead_end_filling {
+}
--- core/src/com/mygdx/game/WorldGenerator/EllersAlgorithm/Maze.java
+++ core/src/com/mygdx/game/WorldGenerator/EllersAlgorithm/Maze.java
@@ -167,5 +167,4 @@ public class Maze {
        // maze.draw();
         maze.solve();
     }
-
 }
